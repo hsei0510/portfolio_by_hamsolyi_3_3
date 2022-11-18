0c06fdb1a92af351c2916c7a04a5fd0a75e42e57
hsei0510 <113411195+hsei0510@users.noreply.github.com>
2022-11-19 02:21:05 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,33 @@
+$(function(){
+
+  /*back-to-top script*/
+  $(window).scroll(function(){
+    let elem = $('.back-to-top');
+    if( $(this).scrollTop() == 0 ){
+      elem.removeClass('on');
+    }else{
+      elem.addClass('on');
+    };
+  });
+
+  /*active script*/
+  let wHeight = $(window).height();
+  $(window).scroll(function(){
+    let thisScrollTop = $(this).scrollTop();
+    $('section').each(function(){
+      let thisOffset = $(this).offset();
+      if( thisOffset.top <= thisScrollTop + 250 && thisOffset.top + wHeight >= thisScrollTop + 250 ){
+        $(this).addClass('active');
+      }
+    });
+  });
+
+  /*Hyperlink scrollTop animate script*/
+  $("nav ul li a,.back-to-top a,.footer-top a,.ani-btn").click(function(e){
+    var thisElem = $(this.hash);
+    $('html,body').stop();
+    $('html,body').animate({scrollTop:thisElem.offset().top},1500);
+    return false;
+  });
+
+})
